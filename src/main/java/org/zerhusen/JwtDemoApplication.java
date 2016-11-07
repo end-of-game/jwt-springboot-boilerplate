8d641338275c88c6651482bdb870e77c791ddbd6
nicolas <zepouet@gmail.com>
2016-11-07 13:47:43 +0100

    Replace CorsFilter by Bean

--- src/main/java/org/zerhusen/JwtDemoApplication.java
+++ src/main/java/org/zerhusen/JwtDemoApplication.java
@@ -2,11 +2,27 @@ package org.zerhusen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class JwtDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JwtDemoApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/auth").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/user").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/persons").allowedOrigins("http://localhost:4200");
+            }
+        };
     }
 }
